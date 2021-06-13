b93476aaba80ab7f0bba85aecfa3eb7b539e2fa8
Edqe14 <configxgt@gmail.com>
2021-06-13 11:43:53 +0700

    dropped

--- src/modules/list.ts
+++ src/modules/list.ts
@@ -3,43 +3,70 @@ import { EventEmitter } from 'events';
 // @ts-ignore
 import { Guilds, Guild } from 'disco-oauth';
 import table from 'tty-table';
+import chalk from 'chalk';
 
-import clamp from '@/utils/clamp';
+// import clamp from '@/utils/clamp';
 
 export default class List extends EventEmitter {
   public guilds: Guilds;
   public names: string[];
   public index = 0;
 
-  public header = [
-    {
-      value: 'Name',
-      align: 'left',
-      width: '20%',
-      headerColor: 'yellow',
-    },
-    {
-      value: 'Info',
-      align: 'left',
-      width: '80%',
-      headerColor: 'yellow',
-      formatter(value: string, index: number): string {
-        console.log(index);
-        if (index === this.index) value = this.style(value, 'bgWhite', 'black');
-        return value;
-      },
-    },
-  ];
+  public defaultHeader = {
+    align: 'left',
+    headerAlign: 'center',
+    headerColor: 'yellow',
+  };
 
   constructor(guilds: Guilds) {
     super();
     this.guilds = guilds;
     this.names = Object.values(guilds.toJSON()).map((g: Guild) => g.name);
 
-    this.showTable();
+    this.showTable(
+      [
+        {
+          value: 'Name',
+          width: '20%',
+          formatter(value, i) {
+            if (i === this.index) return chalk.bgWhite.black(value);
+            return value;
+          },
+          ...this.defaultHeader,
+        },
+        {
+          value: 'Member Count',
+          width: '20%',
+          ...this.defaultHeader,
+        },
+        {
+          value: 'Role Count',
+          width: '20%',
+          ...this.defaultHeader,
+        },
+        {
+          value: 'Info',
+          width: '40%',
+          ...this.defaultHeader,
+        },
+      ],
+      [
+        ['test', ''],
+        ['bruh', ''],
+      ]
+    );
   }
 
-  showTable() {
-    table(this.header);
+  showTable(
+    headers: (string | table.Header | table.Formatter)[],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    body: string[][] | Record<string, any>[],
+    footers?: (string | table.Header | table.Formatter)[],
+    config?: table.Options
+  ): table.Table {
+    const tbl = table(headers, body, footers, config);
+    console.log(tbl.render());
+
+    return tbl;
   }
 }
